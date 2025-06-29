874a36b9ea13a8295ba53f5dde402e64e9439d05
PavPar <paramonpav@yandex.ru>
2025-06-29 11:46:15 +0300

    feat: added linkedhashmap realisation to historymanager, test coverage

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,20 +1,77 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    private final int HISTORY_LIST_MAX_SIZE = 10;
-    private final ArrayList<Task> history = new ArrayList<>();
+    private final HashMap<Integer,Node<Task>> history = new HashMap<>();
+
+    private Node<Task> head;
+    private Node<Task> tail;
 
     @Override
     public ArrayList<Task> getHistory(){
-        return history;
+        ArrayList<Task> result = new ArrayList<>();
+        if(tail == null){
+            return result;
+        }
+        Node<Task> currentNode = tail;
+
+        do{
+            result.add(currentNode.data);
+            currentNode = currentNode.prev;
+        }while(currentNode != null);
+        return  result;
+    }
+
+    @Override
+    public void remove(Task task) {
+        Node<Task> node = history.get(task.getId());
+        if(node == null){
+            return;
+        }
+        history.remove(task.getId());
+
+        Node<Task> previousNode = node.prev;
+        Node<Task> nextNode = node.next;
+
+        if(previousNode != null){
+            previousNode.next = nextNode;
+        }
+
+        if(previousNode == null){
+            head = nextNode;
+        }
+
+        if(nextNode == null){
+            tail = previousNode;
+        }
+
+        if(nextNode != null) {
+            nextNode.prev = previousNode;
+        }
     };
 
     @Override
     public void add(Task task){
-        history.add(0,task);
-        if(history.size() > HISTORY_LIST_MAX_SIZE){
-            history.remove(history.size() -1 );
+        this.removeTaskIfExists(task);
+        Node<Task> newNode =new Node<>(task);
+        history.put(task.getId(),newNode);
+
+        if(head == null || tail == null){
+            head = newNode;
+            tail = newNode;
+            return;
         }
+
+        newNode.prev = tail;
+        tail.next = newNode;
+        tail = newNode;
     }
 
+    private void removeTaskIfExists(Task task){
+        Node<Task> existingTask = history.get(task.getId());
+
+        if(existingTask != null){
+            this.remove(task);
+        }
+    }
 }
--- src/InMemoryHistoryManagerTest.java
+++ src/InMemoryHistoryManagerTest.java
@@ -17,9 +17,116 @@ class InMemoryHistoryManagerTest {
     public void shouldStoreTask(){
         Task task = new Task("test","test",TaskStatus.NEW);
         task.setTaskId(1);
+
         manager.add(task);
+
         ArrayList<Task> tasks = manager.getHistory();
         Assertions.assertTrue(tasks.contains(task));
     }
+
+    @Test
+    public void shouldStoreMultipleTasks(){
+        Task task_1 = new Task("test","test",TaskStatus.NEW);
+        Task task_2 = new Task("test","test",TaskStatus.NEW);
+
+        task_1.setTaskId(1);
+        task_2.setTaskId(2);
+
+        manager.add(task_1);
+        manager.add(task_2);
+
+        ArrayList<Task> tasks = manager.getHistory();
+        Assertions.assertTrue(tasks.contains(task_1));
+        Assertions.assertTrue(tasks.contains(task_2));
+    }
+
+    @Test
+    public void shouldNotHaveDuplicatesOfTasks(){
+        Task task_1 = new Task("test","test",TaskStatus.NEW);
+        Task task_1_clone = new Task("test","test",TaskStatus.NEW);
+
+        task_1.setTaskId(1);
+        task_1_clone.setTaskId(1);
+
+        manager.add(task_1);
+        manager.add(task_1_clone);
+
+        ArrayList<Task> tasks = manager.getHistory();
+
+        Assertions.assertEquals(1, tasks.size());
+    }
+
+    @Test
+    public void shouldRemoveTaskFromArray(){
+        Task task = new Task("test","test",TaskStatus.NEW);
+        task.setTaskId(1);
+        manager.add(task);
+
+        manager.remove(task);
+        ArrayList<Task> tasks = manager.getHistory();
+
+
+        Assertions.assertFalse(tasks.contains(task));
+        Assertions.assertEquals(0,tasks.size());
+    }
+
+    @Test
+    public void shouldRemoveOnlyTaskThatWasProvided(){
+        Task task_1 = new Task("test","test",TaskStatus.NEW);
+        Task task_2 = new Task("test","test",TaskStatus.NEW);
+
+        task_1.setTaskId(1);
+        task_2.setTaskId(2);
+
+        manager.add(task_1);
+        manager.add(task_2);
+
+        manager.remove(task_1);
+        ArrayList<Task> tasks = manager.getHistory();
+
+        Assertions.assertFalse(tasks.contains(task_1));
+        Assertions.assertTrue(tasks.contains(task_2));
+    }
+
+    @Test
+    public void shouldHaveCorrectOrder(){
+        Task task_1 = new Task("test","test",TaskStatus.NEW);
+        Task task_2 = new Task("test","test",TaskStatus.NEW);
+
+        task_1.setTaskId(1);
+        task_2.setTaskId(2);
+
+        manager.add(task_1);
+        manager.add(task_2);
+
+        ArrayList<Task> tasks = manager.getHistory();
+
+        Assertions.assertEquals(0,tasks.indexOf(task_2));
+        Assertions.assertEquals(1,tasks.indexOf(task_1));
+    }
+
+    @Test
+    public void shouldBubbleTaskToTopAfterAdd(){
+        Task task_1 = new Task("test","test",TaskStatus.NEW);
+        Task task_2 = new Task("test","test",TaskStatus.NEW);
+        Task task_3 = new Task("test","test",TaskStatus.NEW);
+
+        task_1.setTaskId(1);
+        task_2.setTaskId(2);
+        task_3.setTaskId(3);
+
+        manager.add(task_1);
+        manager.add(task_2);
+        manager.add(task_3);
+
+        manager.add(task_1);
+
+        ArrayList<Task> tasks = manager.getHistory();
+
+        Assertions.assertEquals(0,tasks.indexOf(task_1));
+        Assertions.assertEquals(1,tasks.indexOf(task_3));
+        Assertions.assertEquals(2,tasks.indexOf(task_2));
+    }
+
 
 }
